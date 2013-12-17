cc8800c91a3e1ab63e1464bc0aa6e2a8e849092c
Eric Sun Lee <ericslee@seas.upenn.edu>
2013-12-17 02:18:11 -0500

    Making the banner page look a bit prettier

--- EventSnap/EditBannerViewController.h
+++ EventSnap/EditBannerViewController.h
@@ -10,15 +10,17 @@
 #import <Parse/Parse.h>
 #import "MBProgressHUD.h"
 
-@interface EditBannerViewController : UIViewController <UITextViewDelegate> {
+@interface EditBannerViewController : UIViewController <UITextViewDelegate, UITextFieldDelegate> {
     MBProgressHUD *HUD;
 }
 
 @property (nonatomic, strong) PFObject *eventObject;
 @property (strong, nonatomic) IBOutlet UIImageView *imageView;
-@property (strong, nonatomic) IBOutlet UITextView *bannerText;
+@property (strong, nonatomic) IBOutlet UITextView *bannerTextView;
+@property (strong, nonatomic) IBOutlet UITextField *bannerText;
 @property (strong, nonatomic) UIImage *bannerComposite;
 @property (strong, nonatomic) NSString *customBannerText;
+//@property (strong, nonatomic)
 
 - (IBAction)saveBanner:(id)sender;
 - (IBAction)clearBanner:(id)sender;
@@ -26,5 +28,6 @@
 - (IBAction)changeToRedBanner:(id)sender;
 - (IBAction)changeToBlueBanner:(id)sender;
 
+- (IBAction)setSentence:(id)sender;
 - (IBAction)closeKeyboard:(id)sender;
 @end
